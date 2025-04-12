69871473a95a90ace66250649d0507a48490122d
igor <134963561+igormiguell@users.noreply.github.com>
2025-04-12 10:36:00 -0300

    added missing license

--- app/src/main/java/com/sevtinge/hyperceiler/utils/PermissionUtils.java
+++ app/src/main/java/com/sevtinge/hyperceiler/utils/PermissionUtils.java
@@ -1,22 +1,21 @@
 /*
  * This file is part of HyperCeiler.
- *
+
  * HyperCeiler is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License.
- *
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
+
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+
  * Copyright (C) 2023-2025 HyperCeiler Contributions
  */
-
 package com.sevtinge.hyperceiler.utils;
 
 import android.Manifest;
@@ -52,7 +51,7 @@ public class PermissionUtils {
         );
     }
 
-    //权限项数组
+    // 权限项数组
     public static final String[] PERMISSIONS = {
             Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.RECORD_AUDIO,
@@ -70,10 +69,10 @@ public class PermissionUtils {
      */
     public interface OnPermissionListener {
 
-        //权限通过
+        // 权限通过
         void onPermissionGranted(Context context);
 
-        //权限拒绝
+        // 权限拒绝
         void onPermissionDenied();
 
     }
@@ -93,13 +92,13 @@ public class PermissionUtils {
         List<String> deniedPermissions = new ArrayList<String>();
         for (int i = 0; i < permissions.length; i++) {
             if (ContextCompat.checkSelfPermission(context, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
-                deniedPermissions.add(permissions[i]);//添加未授予的权限
+                deniedPermissions.add(permissions[i]); // 添加未授予的权限
             }
         }
 
         if (deniedPermissions.size() > 0) {
             mRequestCode = requestCode;
-            //其中请求码范围在[0,65535]
+            // 其中请求码范围在[0,65535]
             ActivityCompat.requestPermissions(context, deniedPermissions.toArray(new String[deniedPermissions.size()]), requestCode);
         } else {
             mOnPermissionListener.onPermissionGranted(context);
@@ -111,7 +110,7 @@ public class PermissionUtils {
      */
     private static boolean verifyPermissions(int[] grantResults) {
         for (int grantResult : grantResults) {
-            if (grantResult != PackageManager.PERMISSION_GRANTED) {//拒绝权限
+            if (grantResult != PackageManager.PERMISSION_GRANTED) { // 拒绝权限
                 System.out.println("grantResult: "+grantResult);
                 return false;
             }
@@ -135,4 +134,3 @@ public class PermissionUtils {
         }
     }
 }
-
